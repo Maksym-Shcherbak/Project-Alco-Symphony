4ba052e24aeb102c9a1a227837230878fbf054b8
Chalaya22 <chalayaolga22@gmail.com>
2023-09-28 20:17:05 +0300

    comit 5

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -1,2 +1,13 @@
 import './header-menu';
-import './header-modal';
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector('[dta-menu-open]'),
+//     closeMenuBtn: document.querySelector('[data-menu-close]'),
+//     menu: document.querySelectorAll('[data-menu]'),
+//   };
+//   refs.openMenuBtn.addEventListener('click', toggleModal);
+//   refs.closeMenuBtn.addEventListener('click', toggleModal);
+//   function toggleModal() {
+//     refs.menu.classList.toggle('is-open');
+//   }
+// })();
