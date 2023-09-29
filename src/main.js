d7dcc71850dc1f5d7f85f8c468e5bbb3bcc6197c
Ferst1 <helenochka4@gmail.com>
2023-09-29 21:38:47 +0200

    notifiction
    Notify lieba

--- src/main.js
+++ src/main.js
@@ -4,4 +4,4 @@ import './js/search/search';
 import './js/cocktails/cocktails';
 import './js/learn-more-btn/learn-more-btn';
 import './js/scroll-anime/scroll-anime';
-
+import './js/notification/notification';
