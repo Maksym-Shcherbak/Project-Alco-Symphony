2ef1010260dc02b783a358a51ba927e5db5a9415
Chalaya22 <chalayaolga22@gmail.com>
2023-10-01 01:44:03 +0300

    comit 2

--- src/js/header/header-dropdown.js
+++ src/js/header/header-dropdown.js
@@ -1,18 +1,15 @@
-import './header-menu';
-import './header-dropdown';
-
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-const dropbtn = document.querySelector('.menu-icon-drop');
+const dropbtn = document.querySelector('.mob-menu-icon-drop');
 dropbtn.addEventListener('click', myFunction);
 function myFunction() {
-  document.getElementById('myDropdown').classList.toggle('show');
+  document.getElementById('mob-myDropdown').classList.toggle('show');
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (e) {
-  if (!e.target.matches('.menu-icon-drop')) {
-    var myDropdown = document.getElementById('myDropdown');
+  if (!e.target.matches('.mob-menu-icon-drop')) {
+    var myDropdown = document.getElementById('mob-myDropdown');
     if (myDropdown.classList.contains('show')) {
       myDropdown.classList.remove('show');
     }
