fc1c2fbd97f2e59edec8cc5da3ce4daf6a0a3b70
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-09-30 17:17:05 +0300

    visible pages

--- src/js/cocktails/getQuantityOfCocktails.js
+++ src/js/cocktails/getQuantityOfCocktails.js
@@ -1,8 +1,17 @@
 export function getQuantityOfCocktails() {
-  let quantity = 0;
+  const quantity = {
+    quantityOfCocktails: 0,
+    quantityOfVisiblePages: 0,
+  };
   if (window.innerWidth < 1280) {
-    return (quantity = 8);
+    quantity.quantityOfCocktails = 8;
   } else {
-    return (quantity = 9);
+    quantity.quantityOfCocktails = 9;
   }
+  if (window.innerWidth < 768) {
+    quantity.quantityOfVisiblePages = 4;
+  } else {
+    quantity.quantityOfVisiblePages = 6;
+  }
+  return quantity;
 }
