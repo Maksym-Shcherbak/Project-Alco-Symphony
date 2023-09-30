ef8e9313bf54d2f9992927b81ee4b57871a7d0c0
Chalaya22 <chalayaolga22@gmail.com>
2023-09-30 22:58:31 +0300

    comit 6

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -1,1 +1,19 @@
 import './header-menu';
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+const dropbtn = document.querySelector('.dropbtn');
+dropbtn.addEventListener('click', myFunction);
+function myFunction() {
+  document.getElementById('myDropdown').classList.toggle('show');
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById('myDropdown');
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+};
