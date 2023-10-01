ca69a3956cc678623f24e5ccdd01231ea447a2d3
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-10-01 06:10:48 +0300

    dark-theme, pagination

--- src/js/CocktailAPI/pagination.js
+++ src/js/CocktailAPI/pagination.js
@@ -6,6 +6,23 @@ export class PaginationForCocktails {
   constructor(container, options) {
     this.totalCards = 0;
     this.options = options;
+    this.options.template = {
+      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+      currentPage:
+        '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+      moveButton:
+        '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</a>',
+      disabledMoveButton:
+        '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</span>',
+      moreButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
+        '<span class="tui-ico-ellip">...</span>' +
+        '</a>',
+    };
     this.pagination = new Pagination(container, this.options);
     this.chunks = [];
   }
