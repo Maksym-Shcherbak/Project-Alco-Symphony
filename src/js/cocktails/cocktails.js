ee5c7f7656a7d5930112a7e02c02bddab14b103b
Foxfonblack <47613239+Foxfonblack@users.noreply.github.com>
2023-10-02 17:28:18 +0300

    favorite-add-none-add

--- src/js/cocktails/cocktails.js
+++ src/js/cocktails/cocktails.js
@@ -2,6 +2,8 @@ import { CocktailsAPI } from '../CocktailAPI/CocktailAPI';
 import { createCocktailCards } from './renderCocktails';
 import { PaginationForCocktails } from '../CocktailAPI/pagination';
 import { getQuantityOfCocktails } from './getQuantityOfCocktails';
+import { loaddedArr } from '../favourite-btn/favourite-btn';
+
 
 const cocktailList = document.querySelector('.cocktails-cards');
 const container = document.getElementById('tui-pagination-container');
@@ -21,7 +23,14 @@ const cocktailsApi = new CocktailsAPI(quantity.quantityOfCocktails);
 
 cocktailsApi
   .getRandomCocktails()
-  .then(data => createCocktailCards(data, cocktailList));
+  .then(data => {
+    createCocktailCards(data, cocktailList)
+  for(let i = 0; i < loaddedArr.lengh; i+=1){
+    if(loaddedArr[i].title === data.drink){
+      console.log(data.drink)
+    }
+  }
+  });
 
 export function renderCocktailsBySearch(arrayOfCocktails) {
   parts = paginationForCocktails.createCardsPerPage(arrayOfCocktails);
--- src/js/favourite-btn/favourite-btn.js
+++ src/js/favourite-btn/favourite-btn.js
@@ -1,30 +1,100 @@
+// import { load } from "../favorite/favorite-coctails-render";
 const coctailsList = document.querySelector('.cocktails-cards');
 
 const arr = [];
 
+function load(key){
+    try {
+        const value = localStorage.getItem(key)
+        return value === null ? undefined : JSON.parse(value)
+    }
+    catch(error){
+        console.log(error.message);
+    }
+  }
+export const loaddedArr = load('favorite')
+console.log(loaddedArr);
 coctailsList.addEventListener('click', favBtnClick);
 
+
+
 function favBtnClick(event) {
-  let svg = null;
-  if (event.target.classList.contains('favoriteIcon')) {
-    svg = event.target;
-  } else if (event.target.classList.contains('cocktail-card-button')) {
-    svg = event.target.children[0];
-  }
-  svg.attributes.fill.value = '#fdfdff';
-  svg.classList.add('colorEnable');
-  console.dir(svg.attributes.fill.value);
-  const object = {
+    if (event.target.tagName==='path'){
+        event.target.parentElement.attributes.fill.value = 'none'
+        event.target.parentElement.classList.remove('enable')
+        loaddedArr.splice(loaddedArr.findIndex(item=>item.id===event.target.closest('li').id),1)
+       
+        console.log(event.target.closest('li').id);
+        console.log(loaddedArr);
+        saveToLocalStorage('favorite', loaddedArr)
+        return
+    }
+    if (event.target.firstElementChild.classList.contains('enable')){
+        event.target.firstElementChild.attributes.fill.value = 'none'
+        event.target.firstElementChild.classList.remove('enable')
+        loaddedArr.splice(loaddedArr.findIndex(item=>item.id===event.target.closest('li').id),1)
+        saveToLocalStorage('favorite', loaddedArr)
+        return
+    }
+    
+//   let svg = null;
+//   if (event.target.classList.contains('favoriteIcon')) {
+//     svg = event.target;
+//   } else if (event.target.classList.contains('cocktail-card-button')) {
+//     svg = event.target.children[0];
+//   }
+console.log(event.target);
+
+//   svg.attributes.fill.value = '#fdfdff';
+//   svg.classList.add('colorEnable');
+//   console.dir(svg.attributes.fill.value);
+if (event.target.classList.contains('cocktail-card-button')){
+    event.target.firstElementChild.attributes.fill.value = '#fdfdff'
+    event.target.firstElementChild.classList.add('enable')
+     const object = {
     id: event.target.closest('li').id,
     img: event.target.closest('li').children[0].children[0].children[0].src,
     title: event.target.closest('li').children[0].children[1].textContent,
     text: event.target.closest('li').children[0].children[2].textContent,
-    favorite: true,
   };
+//   if (arr.length===0){
+//     arr.push(object)
+//     saveToLocalStorage('favorite', arr)
+//   }
+//   if (loaddedArr.length > 0){
+//     loaddedArr.push(object)
+//     saveToLocalStorage('favorite', loaddedArr)
+
+//   }
   arr.push(object);
   console.log(object);
   console.log(arr);
   saveToLocalStorage('favorite', arr);
+}
+else if(event.target.classList.contains('favoriteIcon')){
+    event.target.attributes.fill.value = '#fdfdff'
+    event.target.classList.add('enable')
+    const object = {
+        id: event.target.closest('li').id,
+        img: event.target.closest('li').children[0].children[0].children[0].src,
+        title: event.target.closest('li').children[0].children[1].textContent,
+        text: event.target.closest('li').children[0].children[2].textContent,
+      };
+    //   if (arr.length===0){
+    //     arr.push(object)
+    //     saveToLocalStorage('favorite', arr)
+    //   }
+    //   if (loaddedArr.length>0){
+    //     loaddedArr.push(object)
+    //     saveToLocalStorage('favorite', loaddedArr)
+    
+    //   }
+      arr.push(object);
+      console.log(object);
+      console.log(arr);
+      saveToLocalStorage('favorite', arr);
+}
+ 
 }
 
 function saveToLocalStorage(key, value) {
@@ -36,4 +106,3 @@ function saveToLocalStorage(key, value) {
   }
 }
 
-function removeFavorite(id) {}
