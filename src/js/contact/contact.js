758cd62b7a79b565a8ca26e53db24fa62692888b
IlyRov <127045335+IlyRov@users.noreply.github.com>
2023-10-02 23:43:00 +0100

    WORKING CONTACT PAGE

--- src/js/contact/contact.js
+++ src/js/contact/contact.js
@@ -2,12 +2,16 @@ import '../../js/header/header';
 import '../../js/darktheme/darktheme';
 import '../../js/scroll-anime/scroll-anime';
 
-function initMap() {
-    var uluru = { lat: 50.42496555405263, lng: 30.53408527758109 };
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
-        center: uluru
-    });
+let map;
 
-    var marker = new google.maps.Marker({ position: uluru, map: map });
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: 50.42496555405263, lng: 30.53408527758109 },
+    zoom: 20,
+  });
 }
+
+initMap();
+
