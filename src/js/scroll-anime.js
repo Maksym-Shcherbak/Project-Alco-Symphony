f554c9bef09cf51731395ad494f5b4934a9f6b0a
Elena <36508011+Ferst1@users.noreply.github.com>
2023-10-04 20:20:50 +0200

    scale-no
    change button scale

---
+++ src/js/scroll-anime.js
@@ -0,0 +1,21 @@
+
+
+window.addEventListener('scroll', function() {
+    let button = document.querySelector('.upbtn');
+    if (window.scrollY > 200) {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+});
+
+
+document.querySelector('.upbtn').addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+
+
