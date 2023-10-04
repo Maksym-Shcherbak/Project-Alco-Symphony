e82d8a25d15ab156f284aa26d4789a2c76b567d8
Chalaya22 <chalayaolga22@gmail.com>
2023-10-04 15:19:54 +0300

    comit 2

--- src/js/header/header-mob-dropdown.js
+++ src/js/header/header-mob-dropdown.js
@@ -1,11 +1,13 @@
 const dropbtn = document.querySelector('.mob-dropbtn');
-// const iconDrop = document.querySelector('.mob-menu-icon-drop');
+const iconDrop = document.querySelector('.mob-menu-icon-drop');
 // const iconUp = document.querySelector('.mob-menu-icon-up');
 
 dropbtn.addEventListener('click', myFunction);
 function myFunction(e) {
   if (e.currentTarget.nodeName === 'BUTTON')
     document.getElementById('mob-myDropdown').classList.toggle('show');
+  document.getElementById('mob-icon-drop').classList.toggle('revers');
+
   const contactLink = document.querySelector('.mobile-item.contact');
   contactLink.classList.toggle('show');
 }
@@ -18,21 +20,13 @@ window.onclick = function (e) {
     !e.target.matches('.mob-dropbtn-text')
   ) {
     const myDropdown = document.getElementById('mob-myDropdown');
-    if (myDropdown.classList.contains('show')) {
+    const iconDrop = document.getElementById('mob-icon-drop');
+    if (
+      myDropdown.classList.contains('show') &
+      iconDrop.classList.contains('revers')
+    ) {
       myDropdown.classList.remove('show');
+      iconDrop.classList.remove('revers');
     }
   }
 };
-dropbtn.addEventListener('click', e => {
-  if (
-    !e.target.matches('.mob-dropbtn') &
-    !e.target.matches('.mob-menu-icon-drop') &
-    !e.target.matches('.mob-dropbtn-text')
-  ) {
-    const iconDrop = document.getElementById('mob-icon-drop');
-    const iconUp = document.getElementById('mob-icon-up');
-
-    iconDrop.classList.add('is-hidden');
-    iconUp.classList.remove('is-hidden');
-  }
-});
