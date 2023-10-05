9559a476323cebea7cf1ccd598316e7ec95c71be
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-10-06 00:38:32 +0300

    upd path to fonts

--- src/js/contact/contact.js
+++ src/js/contact/contact.js
@@ -5,9 +5,9 @@ import '../../js/scroll-anime/scroll-anime';
 let map;
 
 async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
+  const { Map } = await google.maps.importLibrary('maps');
 
-  map = new Map(document.getElementById("map"), {
+  map = new Map(document.getElementById('map'), {
     center: { lat: 50.42496555405263, lng: 30.53408527758109 },
     zoom: 20,
   });
@@ -15,3 +15,43 @@ async function initMap() {
 
 initMap();
 
+// (g => {
+//   var h,
+//     a,
+//     k,
+//     p = 'The Google Maps JavaScript API',
+//     c = 'google',
+//     l = 'importLibrary',
+//     q = '__ib__',
+//     m = document,
+//     b = window;
+//   b = b[c] || (b[c] = {});
+//   var d = b.maps || (b.maps = {}),
+//     r = new Set(),
+//     e = new URLSearchParams(),
+//     u = () =>
+//       h ||
+//       (h = new Promise(async (f, n) => {
+//         await (a = m.createElement('script'));
+//         e.set('libraries', [...r] + '');
+//         for (k in g)
+//           e.set(
+//             k.replace(/[A-Z]/g, t => '_' + t[0].toLowerCase()),
+//             g[k]
+//           );
+//         e.set('callback', c + '.maps.' + q);
+//         a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
+//         d[q] = f;
+//         a.onerror = () => (h = n(Error(p + ' could not load.')));
+//         a.nonce = m.querySelector('script[nonce]')?.nonce || '';
+//         m.head.append(a);
+//       }));
+//   d[l]
+//     ? console.warn(p + ' only loads once. Ignoring:', g)
+//     : (d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)));
+// })({
+//   key: 'AIzaSyDC5qC38Dlsq_sEEHeuPB-4KriRluwLxo8',
+//   v: 'weekly',
+//   // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+//   // Add other bootstrap parameters as needed, using camel case.
+// });
