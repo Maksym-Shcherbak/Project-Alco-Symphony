debb5cc89c2f8dbc923ce19f426eabce32f84b3f
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-10-06 20:25:23 +0300

    icon close

--- src/js/modal/renderMarkUpForIngredients.js
+++ src/js/modal/renderMarkUpForIngredients.js
@@ -15,9 +15,9 @@ export function renderModalForIngredient(arr, container) {
       aria-label="close menu review"
       data-first-modal-close
     >
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-        <path d="M18 6L6 18" stroke="#242424" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M6 6L18 18" stroke="#242424" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <path d="M18 6L6 18"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M6 6L18 18"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
     </button>
     <h3 class="ingred-header">${item.title}</h3>
--- src/js/modal/renderMarkupModalForCocktail.js
+++ src/js/modal/renderMarkupModalForCocktail.js
@@ -9,9 +9,9 @@ export function createModalForCocktail(arr, container) {
       aria-label="close menu review"
       data-first-modal-close
     >
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-        <path d="M18 6L6 18" stroke="#242424" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M6 6L18 18" stroke="#242424" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <path d="M18 6L6 18"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M6 6L18 18"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
     </button>
     <div class="cocktail-img-and-ingredients">
